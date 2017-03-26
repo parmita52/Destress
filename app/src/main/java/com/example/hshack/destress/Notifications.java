d3ca85f64970916e5c2e39b0dd25d2d996fbdfa7
neuronote <marilyn.zhang1230@gmail.com>
2017-03-26 09:04:07 -0700

    Intent to Notification Settings specifically

--- app/src/main/java/com/example/hshack/destress/Notifications.java
+++ app/src/main/java/com/example/hshack/destress/Notifications.java
@@ -1,22 +1,30 @@
 package com.example.hshack.destress;
 
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.content.*;
 
 public class Notifications extends AppCompatActivity {
-
-
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notifications);
+
+        Button notificationsSet = (Button) findViewById(R.id.set_notifications);
+        notificationsSet.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v)
+            {
+                startActivity(new Intent(Settings.ACTION_SETTINGS));
+            }
+        });
     }
 
 }
