5450b13d652ebf739c4bee44931bb85e34fea660
neuronote <marilyn.zhang1230@gmail.com>
2017-03-25 23:00:18 -0700

    Added BreatherFocus 2-6

--- app/src/main/java/com/example/hshack/destress/Breather.java
+++ app/src/main/java/com/example/hshack/destress/Breather.java
@@ -20,6 +20,46 @@ public class Breather extends AppCompatActivity {
                 startActivity(new Intent(Breather.this, BreatherFocus1.class));
             }
         });
+
+        ImageButton button2= (ImageButton) findViewById(R.id.imageButton2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.i("clicks", "button 2 has been clicked");
+                startActivity(new Intent(Breather.this, BreatherFocus2.class));
+            }
+        });
+
+        ImageButton button3= (ImageButton) findViewById(R.id.imageButton3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.i("clicks", "button 3 has been clicked");
+                startActivity(new Intent(Breather.this, BreatherFocus3.class));
+            }
+        });
+
+        ImageButton button4= (ImageButton) findViewById(R.id.imageButton4);
+        button4.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.i("clicks", "button 4 has been clicked");
+                startActivity(new Intent(Breather.this, BreatherFocus4.class));
+            }
+        });
+
+        ImageButton button5= (ImageButton) findViewById(R.id.imageButton5);
+        button5.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.i("clicks", "button 5 has been clicked");
+                startActivity(new Intent(Breather.this, BreatherFocus5.class));
+            }
+        });
+
+        ImageButton button6= (ImageButton) findViewById(R.id.imageButton6);
+        button6.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.i("clicks", "button 6 has been clicked");
+                startActivity(new Intent(Breather.this, BreatherFocus6.class));
+            }
+        });
     }
 
 }
