e30edb170577a46879f0a13d221d3e4a36706992
parmita <parmita52@gmail.com>
2017-05-20 14:54:29 -0700

    Commiting before I screw things up with Messages

--- app/src/main/java/com/example/hshack/destress/Feelings.java
+++ app/src/main/java/com/example/hshack/destress/Feelings.java
@@ -8,6 +8,16 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.ibm.watson.developer_cloud.http.ServiceCall;
+import com.ibm.watson.developer_cloud.http.ServiceCallback;
+import com.ibm.watson.developer_cloud.speech_to_text.v1.SpeechToText;
+import com.ibm.watson.developer_cloud.speech_to_text.v1.model.SpeechModel;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Feelings extends AppCompatActivity {
 
@@ -17,7 +27,26 @@ public class Feelings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feelings);
+        SpeechToText service = new SpeechToText();
+        service.setUsernameAndPassword("a35f9438-033e-4810-ad53-c11eaf74b162", "nFNpfYNYTtTq");
+        Map<String, String> headers = new HashMap<String, String>();
+        service.setDefaultHeaders(headers);
 
+        ServiceCall call = service.getModels();
+        call.enqueue(new ServiceCallback<List<SpeechModel>>() {
+            @Override public void onResponse(List<SpeechModel> models) {
+                Toast.makeText(Feelings.this, "working response", Toast.LENGTH_SHORT).show();
+                Log.e("Feelings", "Success");
+            }
+            @Override public void onFailure(Exception e) {
+
+
+                Log.e("Feelings", e.toString());
+            }
+        });
+
+
+        Toast.makeText(this, "service logged in", Toast.LENGTH_SHORT).show();
         mpOcean = MediaPlayer.create(this, R.raw.crisp);
         Button mic = (Button)this.findViewById(R.id.mic_button);
         mic.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/example/hshack/destress/MainActivity.java
+++ app/src/main/java/com/example/hshack/destress/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.hshack.destress;
 
+import android.Manifest;
 import android.content.*;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,6 +12,8 @@ import android.view.*;
 import android.widget.*;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,5 +54,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, Sounds.class));
             }
         });
+
     }
+
 }
